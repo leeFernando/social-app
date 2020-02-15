07ab428d46e8466241d0616d5e2adf3360998ea8
leeFernando <hi.fernando.lee@gmail.com>
2020-02-15 17:18:38 +0700

    add Navbar and Bootstrap CDN

--- twitter-clone-client/src/containers/App.js
+++ twitter-clone-client/src/containers/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { configureStore } from '../store';
+import Navbar from './Navbar';
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div>This is the beginning of a log adventure.</div>
+      <div className='onboarding'>
+        <Navbar />
+      </div>
     </Router>
   </Provider>
 );
--- twitter-clone-client/src/store/reducers/currentUser.js
+++ twitter-clone-client/src/store/reducers/currentUser.js
@@ -5,15 +5,15 @@ const DEFAULT_STATE = {
   user: {}, // all info of the logged in user
 };
 
-export (state = { DEFAULT_STATE }, action) => {
+export default (state = { DEFAULT_STATE }, action) => {
   switch (action.type) {
     case SET_CURRENT_USER:
       return {
         // false if user is empty obj and true otherwise
         isAuthenticated: !!Object.keys(action.user),
-        user: action.user
+        user: action.user,
       };
     default:
       return state;
   }
-}
+};
