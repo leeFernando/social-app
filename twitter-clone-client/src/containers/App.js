bf8493ab0534815efd562064beae6b45d2aead53
leeFernando <hi.fernando.lee@gmail.com>
2020-02-15 16:51:21 +0700

    Create and connect Redux store to React App

--- twitter-clone-client/src/containers/App.js
+++ twitter-clone-client/src/containers/App.js
@@ -1,7 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { configureStore } from '../store';
 
-export default class App extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+const store = configureStore();
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div>This is the beginning of a log adventure.</div>
+    </Router>
+  </Provider>
+);
+
+export default App;
