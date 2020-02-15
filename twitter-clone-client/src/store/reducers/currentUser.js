11de17c1752716ffa4f97dc1992327d8d4bc299d
leeFernando <hi.fernando.lee@gmail.com>
2020-02-15 16:27:19 +0700

    organise frontend folder structure and add actionTypes and errors + currentUser reducers

---
+++ twitter-clone-client/src/store/reducers/currentUser.js
@@ -0,0 +1,19 @@
+import { SET_CURRENT_USER } from '../actionTypes';
+
+const DEFAULT_STATE = {
+  isAuthenticated: false, // true when logged in
+  user: {}, // all info of the logged in user
+};
+
+export (state = { DEFAULT_STATE }, action) => {
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        // false if user is empty obj and true otherwise
+        isAuthenticated: !!Object.keys(action.user),
+        user: action.user
+      };
+    default:
+      return state;
+  }
+}
