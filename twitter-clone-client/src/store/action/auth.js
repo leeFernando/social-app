da1ac36905a76dfc01390ef3290ecc51eaf07474
leeFernando <hi.fernando.lee@gmail.com>
2020-02-17 14:56:19 +0700

    add JWT token to HTTP request header.

--- twitter-clone-client/src/store/action/auth.js
+++ twitter-clone-client/src/store/action/auth.js
@@ -1,17 +1,22 @@
-import { apiCall } from '../../services/api';
+import { apiCall, setTokenHeader } from '../../services/api';
 import { SET_CURRENT_USER } from '../actionTypes';
 import { addError, removeError } from './errors';
 
-function setCurrentUser(user) {
+export function setCurrentUser(user) {
   return {
     type: SET_CURRENT_USER,
     user,
   };
 }
 
+export function setAuthorizationToken(token) {
+  setTokenHeader(token);
+}
+
 export function logout() {
   return dispatch => {
     localStorage.clear();
+    setAuthorizationToken(false);
     dispatch(setCurrentUser({}));
   };
 }
@@ -23,6 +28,7 @@ export function authUser(type, userData) {
       return apiCall('post', `/api/auth/${type}`, userData)
         .then(({ token, ...user }) => {
           localStorage.setItem('jwtToken', token);
+          setAuthorizationToken(token);
           dispatch(setCurrentUser(user));
           dispatch(removeError());
           resolve(); // if the API call succeeded
